f614d1984ff68189c542d3bf25dd211dd7e5512f
ghostfountain <30249360+LandrumTrev@users.noreply.github.com>
2018-08-22 14:35:18 -0400

    initial variables set

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,9 +4,79 @@
 
     // TEMPORARILY PLACE VARIABLE INITIALIZATIONS OUTSIDE jQUERY
 
+    // INITIALIZE ARRAY OF CHARACTER OBJECTS
+
+    let planetsArray = [
+
+        {
+            name: "ARAZIUS",
+            hp: 63,
+            ap: 15,
+            cap: 15,
+            status: "enemy",
+            photo: "<img src='assets/images/arazius.png'>"
+        },
+
+        {
+            name: "IRIDIR",
+            hp: 30,
+            ap: 25,
+            cap: 25,
+            status: "enemy",
+            photo: "<img src='assets/images/iridir.png'>"
+        },
+
+        {
+            name: "KIKINHO",
+            hp: 85,
+            ap: 12,
+            cap: 12,
+            status: "enemy",
+            photo: "<img src='assets/images/kikinho.png'>"
+        },
+
+        {
+            name: "ORLOPIA",
+            hp: 60,
+            ap: 20,
+            cap: 20,
+            status: "enemy",
+            photo: "<img src='assets/images/orlopia.png'>"
+        },
+
+        {
+            name: "RAGEN",
+            hp: 150,
+            ap: 10,
+            cap: 10,
+            status: "enemy",
+            photo: "<img src='assets/images/ragen.png'>"
+        },
+
+        {
+            name: "SIRITH",
+            hp: 90,
+            ap: 15,
+            cap: 15,
+            status: "enemy",
+            photo: "<img src='assets/images/sirith.png'>"
+        },
+
+        {
+            name: "TRILOPE",
+            hp: 110,
+            ap: 20,
+            cap: 20,
+            status: "enemy",
+            photo: "<img src='assets/images/trilope.png'>"
+        },
+
+    ];
+
+
     // INITIALIZE GAME PLAY VARIABLES
 
-    let $enemyName = "Fred";
+    let $enemyName = "ARAZIUS";
 
     let $attackPoints = 8;
 
@@ -15,19 +85,19 @@
 
     // INITIALIZE LIST OF MESSAGES THAT FOLLOW GAME PLAY
 
-    let $startMessage = "Choose your world.";
+    let $startMessage = "Choose a world to be your capital.";
 
-    let $chooseEnemy = "Select your opponent.";
+    let $chooseEnemy = "Select a world to subdue.";
 
-    let $attackInfo = "Click the ATTACK button until one emerges victorious.";
+    let $attackInfo = "Click ATTACK until one world emerges victorious.";
 
-    let $attackResult = "You attacked " + $enemyName + " for " + $attackPoints + " points of damage." + $enemyName + " attacked you for " + $counterAttackPoints + " points of damage.";
+    let $attackResult = "You attacked " + $enemyName + " for " + $attackPoints + " points of damage. " + $enemyName + " attacked you for " + $counterAttackPoints + " points of damage. Attack again.";
 
-    let $defeatMessage = "Your planet has been conquered. Game Over.";
+    let $defeatMessage = "You have been defeated. Game Over.";
 
-    let $winMessage = "You have conquered " + $enemyName + ". Select another opponent.";
+    let $winMessage = "You have conquered " + $enemyName + ". Select another world.";
 
-    let $victoryMessage = "You have conquered all other worlds in your system. Game Over.";
+    let $victoryMessage = "You are the lord of your star system. Game Over.";
 
     let $noEnemy = "You are attacking empty space. Choose a more substantial opponent.";
 
@@ -37,7 +107,7 @@ $(document).ready(function () {
 
     // START JQUERY JS:
 
-
+    $("#message_text").text($startMessage);
 
 
 
